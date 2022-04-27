bbb030483beec4a845a240d067209cda64759c4b
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-04-27 13:16:53 +0530

    fix margin

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -90,7 +90,7 @@
 
 <style lang="postcss">
 nav {
-	@apply mx-auto mt-4 flex max-w-screen-lg items-center justify-between rounded-xl border border-gray-400 px-4 py-2;
+	@apply mx-4 mt-4 flex max-w-screen-md items-center justify-between rounded-xl border border-gray-400 px-4 py-2 md:mx-auto;
 }
 
 a.brand {
