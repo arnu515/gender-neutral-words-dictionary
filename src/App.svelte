68fbc30bedd4747231ccc63585c5069eb8fd9c32
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-04-28 17:21:33 +0530

    load user

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
 import Navbar from '$lib/Navbar.svelte'
+import { loadUser } from '$lib/user.store'
 import Auth from '$pages/Auth.svelte'
 import Index from '$pages/Index.svelte'
+import { onMount } from 'svelte'
 import { Router, Route } from 'svelte-navigator'
+
+let loading = true
+
+onMount(async () => {
+	await loadUser()
+	loading = false
+})
 </script>
 
-<Router>
-	<Navbar />
-	<Route path="/">
-		<Index />
-	</Route>
-	<Route path="/auth">
-		<Auth />
-	</Route>
-</Router>
+{#if !loading}
+	<Router>
+		<Navbar />
+		<Route path="/">
+			<Index />
+		</Route>
+		<Route path="/auth">
+			<Auth />
+		</Route>
+	</Router>
+{/if}
